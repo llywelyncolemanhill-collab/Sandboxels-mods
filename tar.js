533770505951dd77fe901e07aff25e6e0cde4185
llywelyncolemanhill-collab <llywelyn.colemanhill@gmail.com>
2026-08-02 18:00:29 +1000

    Create tar.js
    Adds new liquid

---
+++ tar.js
@@ -0,0 +1,10 @@
+elements.tar = {
+    name: "Tar",
+    color: "#1a1a1a",
+    behavior: "LIQUID",
+    category: "liquids",
+    state: "liquid",
+    density: 1200,
+    viscosity: 5000, // Makes it move super slowly like sludge
+    desc: "Thick sticky sludge."
+};
