173fb357784508043b853d85e49cbd162e16074e
llywelyncolemanhill-collab <llywelyn.colemanhill@gmail.com>
2026-08-08 16:24:39 +1000

    Update behaviors and reactions in breaking_sandboxels.js

--- breaking_sandboxels.js
+++ breaking_sandboxels.js
@@ -1,21 +1,25 @@
 elements.methylamine = {
     color: "#e3f2fd",
-    behavior: behaviors.LIQUID,
+    behavior: "LIQUID",
     category: "liquids"
 };
 
 elements.phenylacetone = {
     color: "#fff9c4",
-    behavior: behaviors.LIQUID,
+    behavior: "LIQUID",
     category: "liquids"
 };
 
 elements.blue_crystals = {
     color: "#00b0ff",
-    behavior: behaviors.POWDER,
+    behavior: "POWDER",
     category: "powders"
 };
 
 reactions.phenylacetone = {
-    methylamine: { elem1: "blue_crystals", elem2: "blue_crystals", chance: 0.8 }
+    "methylamine": { elem1: "blue_crystals", elem2: "blue_crystals", chance: 0.8 }
+};
+
+reactions.methylamine = {
+    "phenylacetone": { elem1: "blue_crystals", elem2: "blue_crystals", chance: 0.8 }
 };
